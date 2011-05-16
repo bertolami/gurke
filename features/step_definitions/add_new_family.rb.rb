f905af5977bf00c517e8aa54069a5645f4a8afe0
rbe <rbe@zuehlke.com>
2011-05-16 16:45:24 +0200

    test.sqlite3

---
+++ features/step_definitions/add_new_family.rb.rb
@@ -0,0 +1,11 @@
+Given /^I am logged in as admin user$/ do
+  # ignored currently
+end
+
+When /^I create a schwachzehrend Schmetterlingsblütler$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the newly created  Family is available for use$/ do
+  pending # express the regexp above with the code you wish you had
+end
