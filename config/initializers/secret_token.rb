d3f2d2ef02afe953936b93c945668e62f86fbe09
ubuntu <ubuntu@ubuntu-camp-box.(none)>
2011-05-16 12:14:31 +0200

    initiales gurke commti

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gurke::Application.config.secret_token = '4b88c64a9ff285561384c4523fda008185dc74c909b178ed38ee349356668ba41c838f63610d95b4c8f7149b4bf0d2136f33dbd3c8c077a9a6b0b5fd5ccd94cb'
