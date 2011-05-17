0b269b60e42e86b88367abde659381f0f34053f3
muc <muc>
2011-05-17 18:04:51 +0200

    added some steps to the webrat driver

--- features/application_driver_layers/webrat_driver.rb
+++ features/application_driver_layers/webrat_driver.rb
@@ -1,5 +1,6 @@
 module WebratLayer
 
+
   def create_new_family(family, energy)
     visit new_family_path
     fill_in "Name", :with => family
@@ -8,7 +9,21 @@ module WebratLayer
   end
 
   def find_family(family_name)
-      visit families_path
-      response
+    visit families_path
+    response
   end
+
+  def create_new_plant(plant_name, family_name)
+    visit new_plant_path
+    fill_in "Name", :with => plant_name
+    select family_name, :from => "plant[family_id]"
+    click_button "Create Plant"
+  end
+
+  def find_plant(name)
+    visit plants_path
+    response
+  end
+
+
 end
--- features/step_definitions/add_new_plant.rb
+++ features/step_definitions/add_new_plant.rb
@@ -1,23 +1,20 @@
 Given /^I am logged in as "([^"]*)" user$/ do |arg1|
- # do nothing yet
+  # do nothing yet
 end
 
-Given /^the family "([^"]*)" is already known$/ do |family|
-  visit families_path
-  response.should contain(family)
+Given /^the family "([^"]*)" is already known$/ do |family_name|
+  family = find_family(family_name)
+  family.should contain(family_name)
 end
 
-When /^I create a plant "([^"]*)" with family "([^"]*)"$/ do |name, family_name|
-  visit new_plant_path
-  fill_in "Name", :with => name
-  select family_name, :from => "plant[family_id]"
-  click_button "Create Plant"
+When /^I create a plant "([^"]*)" with family "([^"]*)"$/ do |plant_name, family_name|
+  @response = create_new_plant(plant_name, family_name)
 end
 
-Then /^the newly created plant "([^"]*)" is available for use$/ do |plant|
-  response.should contain(plant)
+Then /^the newly created plant "([^"]*)" is available for use$/ do |plant_name|
+   @response.should contain(plant_name)
 end
 
 Then /^belongs to the family "([^"]*)"$/ do |family|
-  response.should contain(family)
+  @response.should contain(family)
 end
