4200a27e25c04acd79b5f1cd4572ad98eff5441f
rbe <rbe@zuehlke.com>
2011-05-17 14:28:06 +0200

    edit plant

--- features/step_definitions/edit_plant.rb
+++ features/step_definitions/edit_plant.rb
@@ -7,7 +7,7 @@ end
 
 When /^I rename the plant to "([^"]*)"$/ do |new_name|
   @new_name = new_name
-  click_link_within "div[id*=\""+@original_name+"\"]", "Edit"
+  click_edit  @original_name
   fill_in "Name", :with => @new_name
   click_button
 end
@@ -16,4 +16,23 @@ Then /^the plant is available under the new name$/ do
   response.should contain(@new_name)
   visit plants_path
   response.should contain(@new_name)
+end
+
+When /^I change the family to "([^"]*)"$/ do |new_family_name|
+  @new_family_name = new_family_name
+  click_edit  @original_name
+  select @new_family_name
+  click_button
+end
+
+Then /^the family of the plant is changed$/ do
+  @response.should contain @new_family_name
+end
+
+Then /^the family contains the newly changed plant$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+def click_edit(plant_name)
+  click_link_within "div[id*=\""+plant_name+"\"]", "Edit"
 end
