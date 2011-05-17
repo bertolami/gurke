01948264232786b2e112c1d68276f4a1f36299e4
rbe <rbe@zuehlke.com>
2011-05-17 14:51:33 +0200

    additional Then added

--- features/edit_plant.feature
+++ features/edit_plant.feature
@@ -8,11 +8,12 @@ Feature: Edit a plant
     And the plant "Rucola" is already known
     When I rename the plant to "Rucoli"
     Then the plant is available under the new name
-
+ 
  Scenario: Change the family of Kopfsalat to Gaensefussgewaechse
     Given I am logged in as "admin" user
     And the plant "Kopfsalat" is already known
-    When I change the family to "Gaensefussgewaechse"
+    When I change the family from "Koerbchenbluetler" to "Gaensefussgewaechse"
     Then the family of the plant is changed
     And the family contains the newly changed plant
+    And the old family does not contains the newly changed plant anymore
  
--- features/step_definitions/edit_plant.rb
+++ features/step_definitions/edit_plant.rb
@@ -2,7 +2,6 @@ Given /^the plant "([^"]*)" is already known$/ do |original_name|
   @original_name = original_name
   visit plants_path
   response.should contain(@original_name)
-  response.should have_selector( "div[id*=\""+@original_name+"\"]")
 end
 
 When /^I rename the plant to "([^"]*)"$/ do |new_name|
@@ -18,7 +17,8 @@ Then /^the plant is available under the new name$/ do
   response.should contain(@new_name)
 end
 
-When /^I change the family to "([^"]*)"$/ do |new_family_name|
+When /^I change the family from "([^"]*)" to "([^"]*)"$/ do |original_family_name, new_family_name|
+  @original_family_name = original_family_name
   @new_family_name = new_family_name
   click_edit  @original_name
   select @new_family_name
@@ -34,6 +34,13 @@ Then /^the family contains the newly changed plant$/ do
   click_show @new_family_name
   response.should contain(@original_name)
 end
+
+Then /^the old family does not contains the newly changed plant anymore$/ do
+  visit families_path
+  click_show @original_family_name
+  response.should_not contain @original_name
+end
+
 
 def click_edit(name)
   click_link_within "div[id*=\""+name+"\"]", "Edit"
