8369f91d0d93a6010e5f4a2a3c2f5f4bb60f7037
rbe <rbe@zuehlke.com>
2011-05-18 11:56:58 +0200

    test fixed

--- test/unit/beds_test.rb
+++ test/unit/beds_test.rb
@@ -4,6 +4,6 @@ class BedsTest < ActiveSupport::TestCase
   test "given a bed in state A, suggest plants that fit" do
     bed = Bed.new("A")
     plants = bed.suggest_plants
-    assert_equal(%w(Kopfsalat Randen Spinat Zwiebel).sort, (plants.collect { |item| item.name }).sort)
+    assert_equal(%w(Kopfsalat Randen Spinat Zwiebel Rüebli).sort, (plants.collect { |item| item.name }).sort)
   end
 end
